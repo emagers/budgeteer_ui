bcf94605871ee397dee40e4d2900dd80ce5a73d2
= <eric.magers15@gmail.com>
2024-12-23 10:14:17 -0800

    Makes sure picked colors contrast well with white background

--- src/App.js
+++ src/App.js
@@ -56,49 +56,81 @@ function NavBar() {
 		i18n.changeLanguage(i18n.language === 'en' ? 'br' : 'en');
 	}
 
+	const flagemojiToPNG = (flag) => {
+		return (<img src={`https://flagcdn.com/24x18/${flag}.png`} alt='flag' />)
+	}
+
+	const getFlagEmoji = (lang) => {
+		switch (lang) {
+			case 'en':
+				return 'br';
+			case 'br':
+				return 'us';
+			default:
+				return 'us';
+		}
+	};
+
 	return (
 		<div class="navbar">
 			<div class="navbar-div">{t('navbar_welcome')} {name}!</div>
 			<div class="navbar-div"><a href="/logout">logout</a></div>
 			<div class="navbar-div">
-				<button onClick={on_switch}>
-					{t('navbar_switch_language')}
-				</button>
+				<div onClick={on_switch} class="navbar-button">
+					<span>
+						{flagemojiToPNG(getFlagEmoji(i18n.language))}
+					</span>
+					<span>
+						{t('navbar_switch_language')}
+					</span>
+				</div>
 			</div>
 		</div>
 	)
 }
 
-const COLORS = [
-	'#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-	'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-	'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399'
-];
-/// Create a list of 30 colors that are a much whiter version of the colors above, they should correspond to the colors above in order
-const LIGHTER_COLORS = COLORS.map(color => {
-	const colorObj = new Option().style;
-	colorObj.color = color;
-	const rgb = colorObj.color.match(/\d+/g).map(Number);
-	return `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 0.3)`;
-});
+// const COLORS = [
+// 	'#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+// 	'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+// 	'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399'
+// ];
+// /// Create a list of 30 colors that are a much whiter version of the colors above, they should correspond to the colors above in order
+// const LIGHTER_COLORS = COLORS.map(color => {
+// 	const colorObj = new Option().style;
+// 	colorObj.color = color;
+// 	const rgb = colorObj.color.match(/\d+/g).map(Number);
+// 	return `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, 0.3)`;
+// });
 
-const COMPLEMENTARY_COLORS = [
-	'#00CCFF', '#004C99', '#00CC00', '#000066', '#FF4C4C', '#1A66FF', '#FFCC00', '#666600', '#660066', '#4DFF4D',
-	'#FF4D4D', '#FF6600', '#1A1AFF', '#FFB3B3', '#FFCC99', '#00FF99', '#0033CC', '#00FFCC', '#FF9933', '#CCFF33',
-	'#FF66CC', '#FF1A1A', '#00FF66', '#FF3300', '#FF99FF', '#FFCC66', '#00FF33', '#FF66FF', '#FF4DFF', '#FF1AFF'
-];
+// const COMPLEMENTARY_COLORS = [
+// 	'#00CCFF', '#004C99', '#00CC00', '#000066', '#FF4C4C', '#1A66FF', '#FFCC00', '#666600', '#660066', '#4DFF4D',
+// 	'#FF4D4D', '#FF6600', '#1A1AFF', '#FFB3B3', '#FFCC99', '#00FF99', '#0033CC', '#00FFCC', '#FF9933', '#CCFF33',
+// 	'#FF66CC', '#FF1A1A', '#00FF66', '#FF3300', '#FF99FF', '#FFCC66', '#00FF33', '#FF66FF', '#FF4DFF', '#FF1AFF'
+// ];
 
 function GetRandomColors(num) {
+
 	const colors = {
 		backgroundColors: [],
 		lightColors: [],
 		complementaryColors: []
 	};
 	for (let i = 0; i < num; i++) {
-		const idx = Math.floor(Math.random() * COLORS.length);
-		colors.backgroundColors.push(COLORS[idx]);
-		colors.lightColors.push(LIGHTER_COLORS[idx]);
-		colors.complementaryColors.push(COMPLEMENTARY_COLORS[idx]);
+// There should never be any duplicates in the random colors picked, and there should be enough contrast against a white background.
+		let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+		// Remove the hash at the start if it's there
+		let hex = color.replace(/^#/, '');
+
+		// Parse the r, g, b values
+		let bigint = parseInt(hex, 16);
+		let r = (bigint >> 16) & 255;
+		let g = (bigint >> 8) & 255;
+		let b = bigint & 255;
+
+		colors.backgroundColors.push(color);
+		colors.lightColors.push(`rgba(${r}, ${g}, ${b}, 0.3)`);
+		colors.complementaryColors.push(`#${(0xFFFFFF - parseInt(hex, 16)).toString(16)}`);
 	}
 	return colors;
 }
